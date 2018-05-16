8042628bb1e370face79970982155206354ca8d3
0xB9 <0xB9@protonmail.com>
2018-05-15 23:01:56 -0400

    Update Variables

--- MyBBscan.py
+++ MyBBscan.py
@@ -14,17 +14,17 @@ class color:
 files = ['mytabs.php','usersocial.php','myawards.php','myarcade.php','tlink.php','mybbirlastthreadsprofile.php','downloads.php',
 		'recentthread.php','mc_craft.php','lpop.php', 'adminnotes.php']
 
-mytabs = ["MyTabs", "1.32", "SQL Injection", "https://www.exploit-db.com/exploits/17595/"]
-socialnetworks = ["User Social Networks", "1.2", "Cross-Site Scripting", "https://www.exploit-db.com/exploits/34539/"]
-myawards = ["MyAwards", "2.3", "CSRF", "https://www.exploit-db.com/exploits/39290/"]
-myarcade = ["My Arcade", "1.3", "Cross-Site Scripting", "https://www.exploit-db.com/exploits/44186/"]
-tlink = ["Threads to Link", "1.3", "Cross-Site Scripting", "https://www.exploit-db.com/exploits/44547/"]
-lastthreadprofile = ["Last User's Threads in Profile", "1.2", "Cross-Site Scripting", "https://www.exploit-db.com/exploits/44339/"]
+myTabs = ["MyTabs", "1.32", "SQL Injection", "https://www.exploit-db.com/exploits/17595/"]
+socialNetworks = ["User Social Networks", "1.2", "Cross-Site Scripting", "https://www.exploit-db.com/exploits/34539/"]
+myAwards = ["MyAwards", "2.3", "CSRF", "https://www.exploit-db.com/exploits/39290/"]
+myArcade = ["My Arcade", "1.3", "Cross-Site Scripting", "https://www.exploit-db.com/exploits/44186/"]
+threadToLink = ["Threads to Link", "1.3", "Cross-Site Scripting", "https://www.exploit-db.com/exploits/44547/"]
+lastThreadProfile = ["Last User's Threads in Profile", "1.2", "Cross-Site Scripting", "https://www.exploit-db.com/exploits/44339/"]
 downloads = ["Downloads", "2.0.3", "Cross-Site Scripting", "https://www.exploit-db.com/exploits/44400/"]
 recentThreadIndex = ["Recent Threads On Index", "17.0", "Cross-Site Scripting", "https://www.exploit-db.com/exploits/44420/"]
-mc_mycode = ["Minecraft Crafting MyCode", "1.2", "Cross-Site Scripting", "[item=10;item da craftare]\')\"onclick=\"alert(\'test\');\" [/item]"]
-lpop = ["Latest Post on Profile", "1.1", "Cross-Site Scripting", "https://www.exploit-db.com/exploits/44608/"]
-adminnotes = ["Admin Notes", "1.1", "CSRF", "awaiting..."]
+minecraftMyCode = ["Minecraft Crafting MyCode", "1.2", "Cross-Site Scripting", "[item=10;item da craftare]\')\"onclick=\"alert(\'test\');\" [/item]"]
+latestPostProfile = ["Latest Post on Profile", "1.1", "Cross-Site Scripting", "https://www.exploit-db.com/exploits/44608/"]
+adminNotes = ["Admin Notes", "1.1", "CSRF", "awaiting..."]
 
 def banner():
 	print color.green+"MyBBscan\n"+color.end+"By: "+color.blue+"0xB9 "+color.end+"from "+color.blue+"https://LuxorForums.com\n"+color.end
@@ -51,45 +51,45 @@ def scan(site, upload):
 	if status == 200:
 		if upload == "mytabs.php":
 			print color.yellow+"[*] Possible Vulnerable Plugin!"+color.end
-			print "Vulnerable Plugin: "+mytabs[0]
-			print "Version: "+mytabs[1]
-			print "Vulnerability Type: "+mytabs[2]
-			print "Information: "+mytabs[3]
+			print "Vulnerable Plugin: "+myTabs[0]
+			print "Version: "+myTabs[1]
+			print "Vulnerability Type: "+myTabs[2]
+			print "Information: "+myTabs[3]
 			print "-"*60
 		elif upload == "usersocial.php":
 			print color.yellow+"[*] Possible Vulnerable Plugin!"+color.end
-			print "Vulnerable Plugin: "+socialnetworks[0]
-			print "Version: "+socialnetworks[1]
-			print "Vulnerability Type: "+socialnetworks[2]
-			print "Information: "+socialnetworks[3]
+			print "Vulnerable Plugin: "+socialNetworks[0]
+			print "Version: "+socialNetworks[1]
+			print "Vulnerability Type: "+socialNetworks[2]
+			print "Information: "+socialNetworks[3]
 			print "-"*60
 		elif upload == "myawards.php":
 			print color.yellow+"[*] Possible Vulnerable Plugin!"+color.end
-			print "Vulnerable Plugin: "+myawards[0]
-			print "Version: "+myawards[1]
-			print "Vulnerability Type: "+myawards[2]
-			print "Information: "+myawards[3]
+			print "Vulnerable Plugin: "+myAwards[0]
+			print "Version: "+myAwards[1]
+			print "Vulnerability Type: "+myAwards[2]
+			print "Information: "+myAwards[3]
 			print "-"*60
 		elif upload == "myarcade.php":
 			print color.yellow+"[*] Possible Vulnerable Plugin!"+color.end
-			print "Vulnerable Plugin: "+myarcade[0]
-			print "Version: "+myarcade[1]
-			print "Vulnerability Type: "+myarcade[2]
-			print "Information: "+myarcade[3]
+			print "Vulnerable Plugin: "+myArcade[0]
+			print "Version: "+myArcade[1]
+			print "Vulnerability Type: "+myArcade[2]
+			print "Information: "+myArcade[3]
 			print "-"*60
 		elif upload == "tlink.php":
 			print color.yellow+"[*] Possible Vulnerable Plugin!"+color.end
-			print "Vulnerable Plugin: "+tlink[0]
-			print "Version: "+tlink[1]
-			print "Vulnerability Type: "+tlink[2]
-			print "Information: "+tlink[3]
+			print "Vulnerable Plugin: "+threadToLink[0]
+			print "Version: "+threadToLink[1]
+			print "Vulnerability Type: "+threadToLink[2]
+			print "Information: "+threadToLink[3]
 			print "-"*60
 		elif upload == "mybbirlastthreadsprofile.php":
 			print color.yellow+"[*] Possible Vulnerable Plugin!"+color.end
-			print "Vulnerable Plugin: "+lastthreadsprofile[0]
-			print "Version: "+lastthreadsprofile[1]
-			print "Vulnerability Type: "+lastthreadsprofile[2]
-			print "Information: "+lastthreadsprofile[3]
+			print "Vulnerable Plugin: "+lastThreadsProfile[0]
+			print "Version: "+lastThreadsProfile[1]
+			print "Vulnerability Type: "+lastThreadsProfile[2]
+			print "Information: "+lastThreadsProfile[3]
 			print "-"*60
 		elif upload == "downloads.php":
 			print color.yellow+"[*] Possible Vulnerable Plugin!"+color.end
@@ -104,27 +104,27 @@ def scan(site, upload):
 			print "Version: "+recentThreadIndex[1]
 			print "Vulnerability Type: "+recentThreadIndex[2]
 			print "Information: "+recentThreadIndex[3]
-			print "-"*60	
+			print "-"*60
 		elif upload == "mc_craft.php":
 			print color.yellow+"[*] Possible Vulnerable Plugin!"+color.end
-			print "Vulnerable Plugin: "+mc_mycode[0]
-			print "Version: "+mc_mycode[1]
-			print "Vulnerability Type: "+mc_mycode[2]
-			print "Information: "+mc_mycode[3]
+			print "Vulnerable Plugin: "+minecraftMyCode[0]
+			print "Version: "+minecraftMyCode[1]
+			print "Vulnerability Type: "+minecraftMyCode[2]
+			print "Information: "+minecraftMyCode[3]
 			print "-"*60
 		elif upload == "lpop.php":
 			print color.yellow+"[*] Possible Vulnerable Plugin!"+color.end
-			print "Vulnerable Plugin: "+lpop[0]
-			print "Version: "+lpop[1]
-			print "Vulnerability Type: "+lpop[2]
-			print "Information: "+lpop[3]
+			print "Vulnerable Plugin: "+latestPostProfile[0]
+			print "Version: "+latestPostProfile[1]
+			print "Vulnerability Type: "+latestPostProfile[2]
+			print "Information: "+latestPostProfile[3]
 			print "-"*60
 		elif upload == "adminnotes.php":
 			print color.yellow+"[*] Possibble Vulnerable Plugin!"+color.end
-			print "Vulnerable Plugin: "+adminnotes[0]
-			print "Version: "+adminnotes[1]
-			print "Vulnerability Type: "+adminnotes[2]
-			print "Information: "+adminnotes[3]
+			print "Vulnerable Plugin: "+adminNotes[0]
+			print "Version: "+adminNotes[1]
+			print "Vulnerability Type: "+adminNotes[2]
+			print "Information: "+adminNotes[3]
 			print "-"*60
 		else:
 			pass
