5a7e6221aaf69f4cdd4af600bbe46e747a4af560
0xB9 <0xB9@protonmail.com>
2023-04-23 20:42:51 -0400

    Change variable

--- version_check.py
+++ version_check.py
@@ -2,8 +2,8 @@ import requests
 import json
 
 currentVersion = "v3.0"
-repo_url = "https://api.github.com/repos/0xB9/MyBBscan/releases/latest"
+repo = "https://api.github.com/repos/0xB9/MyBBscan/releases/latest"
 
-response = requests.get(repo_url)
+response = requests.get(repo)
 release = json.loads(response.text or response.content)
 latestVersion = release["tag_name"]
